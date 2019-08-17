b5c77fb46ab830d8f50fb787a83838fe5e5088bb
W-Nakasone <psyqhical@icloud.com>
2019-08-17 21:59:36 +0900

    スイッチの追加　タイトルラベルの除去

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -13,23 +13,31 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate, UIWebViewDelegate {
     
     
+    @IBOutlet weak var uiswitch1: UISwitch!
     
     @IBOutlet weak var label1: UILabel!
     
     
     @IBOutlet weak var webview1: WKWebView!
     
-    let initialUrl = NSURL(string: "https://www.google.co.jp") //任意のURL
+    let initialUrl = NSURL(string: "https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwixocKi94nkAhWhxYsBHQyLBK4QFjAAegQIBRAB&url=https%3A%2F%2Fwww.google.com%2Fmaps%2F%4035.681983%2C139.773973%2C15z%3Fhl%3Dja&usg=AOvVaw2e6mcj_CToBXHyZ0odG35B") //任意のURL
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = NSURL(string: "https://www.google.com") {
+        if let url = NSURL(string: "https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwixocKi94nkAhWhxYsBHQyLBK4QFjAAegQIBRAB&url=https%3A%2F%2Fwww.google.com%2Fmaps%2F%4035.681983%2C139.773973%2C15z%3Fhl%3Dja&usg=AOvVaw2e6mcj_CToBXHyZ0odG35B") {
             let request = NSURLRequest(url: url as URL)
           webview1.load(request as URLRequest)
+        }
         
+        if(uiswitch1.isOn)
+        {
+            // Switchがonの時の処理
+        }
+        else
+        {
+            // Switchがoffの時の処理
+        }
         
-       
-    }
     
     
 }
